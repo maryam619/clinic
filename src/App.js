b71895452244b4cc95be02bb22789a1d13383118
maryam619 <m.farokhi94@gmail.com>
2025-05-09 16:54:22 +0330

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>اپلیکیشن کلینیک</h1>
+      <p>این نسخه ابتدایی برای بارگذاری در GitHub است.</p>
+    </div>
+  );
+}
+
+export default App;
